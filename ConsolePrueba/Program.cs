97a6f89df6417ae2492e8013c4de36d81e8ee391
José Alberto García <jagarcias1978@gmail.com>
2022-09-19 10:48:44 +0200

    Se instancia un objeto

--- ConsolePrueba/Program.cs
+++ ConsolePrueba/Program.cs
@@ -7,6 +7,8 @@ namespace ConsolePrueba
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //Vamos a instanciar un objeto de la clase Class1
+            Class1 objeto = new Class1();
             Console.ReadKey();
         }
     }
